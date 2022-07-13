b45be396924a50f5c4497eb7ecc23c8fb76b2389
kazuoki.tokuno <kazuoki.tokuno@tamu.edu>
2022-07-13 17:50:25 -0400

    adding weekend calculations

--- backend.py
+++ backend.py
@@ -1,8 +1,18 @@
-from datetime import date
+from datetime import date, timedelta
+import holidays
 
-def find_days(begin, end):
-    delta = end - begin
-    print(delta.days)
+def find_delta(begin, end):
+    delta = (end - begin).days
+    return delta
+
+def subtract_weekends(begin,end,total):
+    weekdays = total - int(total/7) * 2
+    #Check the remaining days (<7) from the end date for weekends.
+    for i in range(total % 7):
+        dayOfWeek = (end - timedelta(days = i)).weekday()
+        if(dayOfWeek == 5 or dayOfWeek == 6):
+            weekdays -= 1
+    return weekdays
 
 def enter_date():
     year = int(input("Year: "))
@@ -12,8 +22,13 @@ def enter_date():
 
 
 one = date(2020,7,5)
-two = date(2020,7,7)
+two = date(2022,7,10)
 
 
-hey = enter_date()
-find_days(hey,two)
+#hey = enter_date()
+#total = find_delta(hey,two)
+#subtract_weekends(hey,two,total)
+us = holidays.US()
+
+for ptr in holidays.US(years = 2022).items():
+    print(ptr)
