455f06e5d27c6e805db23b82efcd5aedfcc84a7b
kztok-Desktop <kazuoki.tokuno@gmail.com>
2022-08-01 16:58:06 -0400

    flask

--- backend.py
+++ backend.py
@@ -1,18 +1,23 @@
+from flask import Flask, render_template
 from stock_holiday import *
 
-def enter_date():
-    year = int(input("Year: "))
-    month = int(input("Month: "))
-    day = int(input("Day: "))
-    return date(year,month,day)
+# def enter_date():
+#     year = int(input("Year: "))
+#     month = int(input("Month: "))
+#     day = int(input("Day: "))
+#     return date(year,month,day)
 
-#hey = enter_date()
+# # for i in range(2019,2024):
+# #     print(i+1)
+# one = date(2022,5,1)
+# two = date(2022,5,5)
+# print("total: ")
+# # print(calculate_trading_days(one,two))
+# # print(calculate_future_stock_date(two,calculate_trading_days(one,two)))
+# print(calculate_future_stock_date(two,58))
 
-# for i in range(2019,2024):
-#     print(i+1)
-one = date(2022,5,11)
-two = date(2022,5,27)
-print("total: ")
-print(calculate_trading_days(one,two))
-print(calculate_future_stock_date(two,calculate_trading_days(one,two)))
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
