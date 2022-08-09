ac70d5e1a4bf083e6a93d5165acb565f627b25fc
kztok-Desktop <kazuoki.tokuno@gmail.com>
2022-08-09 16:42:26 -0400

    testing yfinance,pandas,plotly

--- test.py
+++ test.py
@@ -2,7 +2,11 @@
 from prog.stock_holiday import *
 from prog.format_helper import *
 import yfinance as yf
+import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as py
+
+
 
 # print(date_format_conversion("2011-1-1"))
 
@@ -11,6 +15,19 @@ import matplotlib.pyplot as plt
 # readable_date(one)
 
 msft = yf.Ticker("MSFT")
-wer = msft.history(period="2d")
-print(wer[""][0])
-print(wer)
+wer = msft.history(period="2y")
+# print(wer[["Low","High"]])
+wer["Avg"] = round((wer["Low"] + wer["High"])/2,2)
+asd = wer["Avg"]
+# print(wer["Avg"][6])
+# for i in range(20):
+#     print(wer["Avg"][i])
+# f = open("test.txt","w")
+# f.write(str(asd))
+# f.close()
+# asd.to_csv('out.csv')
+df = pd.read_csv('out.csv')
+sh = py.line(x=df['Date'],y=df['Avg'])
+sh.show()
+# plt.plot(df['Date'],df['Avg'])
+# plt.show()
