f18eece1fc48c051348bc832a53acdf678c10139
kztok-Desktop <kazuoki.tokuno@gmail.com>
2022-08-08 22:23:49 -0400

    testing yfinance and matplotlib

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 
 from prog.stock_holiday import *
 from prog.format_helper import *
+import yfinance as yf
+import matplotlib.pyplot as plt
 
-print(date_format_conversion("2011-1-1"))
+# print(date_format_conversion("2011-1-1"))
 
-one = date(2022,1,1)
+# one = date(2022,1,1)
 
-readable_date(one)
+# readable_date(one)
+
+msft = yf.Ticker("MSFT")
+wer = msft.history(period="2d")
+print(wer[""][0])
+print(wer)
